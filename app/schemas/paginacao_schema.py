a3f9486b0c6788f352fea693dc22af36ec47012b
osurinhu <antoniodepaduarabelo767@gmail.com>
2026-05-28 15:06:12 -0300

    Contador de itens em pagina

--- app/schemas/paginacao_schema.py
+++ app/schemas/paginacao_schema.py
@@ -1,7 +1,14 @@
 from marshmallow import Schema, fields
 
 class PaginadoOutputModel(Schema):
-        page = fields.Int()
-        per_page = fields.Int()
-        total = fields.Int()
-        has_next = fields.Bool()
+	page = fields.Int()
+	per_page = fields.Int()
+	total = fields.Int()
+	has_next = fields.Bool()
+	item_count = fields.Method('item_counter')
+
+	def item_counter(self, obj) -> int:
+		try:
+			return len(obj.items)
+		except:
+			return None
